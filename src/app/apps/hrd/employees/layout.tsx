d49ad8a2b1634dd2b0212b2bf73505ee7773225f
BTech LLC <info@btech.am>
2023-02-12 00:51:05 +0400

    Update

--- src/app/apps/hrd/employees/layout.tsx
+++ src/app/apps/hrd/employees/layout.tsx
@@ -13,7 +13,7 @@ export default function EmployeesLayout({
   return (
     <div style={{padding: '30px', backgroundColor: 'rgba(0,144,255,0.3)'}}>
       <h1>Employees Layout</h1>
-      <button onClick={onClick} style={{padding: '10px 20px', backgroundColor: 'rgba(0,144,255,0.3)'}}>
+      <button onClick={onClick} style={{padding: '10px 20px', marginBottom: '20px', backgroundColor: 'rgba(0,144,255,0.3)'}}>
         {open ? 'Open' : 'Close'}
       </button>
       {children}
