28190542c589e77b820bc7aa9e2ae4b2fe84a651
BTech LLC <info@btech.am>
2023-02-12 00:50:23 +0400

    Update

--- src/app/apps/hrd/employees/layout.tsx
+++ src/app/apps/hrd/employees/layout.tsx
@@ -9,9 +9,13 @@ export default function EmployeesLayout({
   children: ReactNode
 }) {
   const [open, setOpen] = useState<boolean>(false)
+  const onClick = () => setOpen(!open)
   return (
     <div style={{padding: '30px', backgroundColor: 'rgba(0,144,255,0.3)'}}>
       <h1>Employees Layout</h1>
+      <button onClick={onClick} style={{padding: '10px 20px', backgroundColor: 'rgba(0,144,255,0.3)'}}>
+        {open ? 'Open' : 'Close'}
+      </button>
       {children}
     </div>
   )
