93eb8efb63f54cbbf5e10ddf579ca2bcd829d5bf
BTech LLC <info@btech.am>
2023-02-12 00:54:48 +0400

    Update

--- src/app/apps/hrd/employees/layout.tsx
+++ src/app/apps/hrd/employees/layout.tsx
@@ -1,22 +1,15 @@
-'use client'
-
 import type {ReactNode} from 'react'
-import {useState}       from 'react'
+import AppLayout        from '@/app/apps/AppLayout'
 
 export default function EmployeesLayout({
   children,
 }: {
   children: ReactNode
 }) {
-  const [open, setOpen] = useState<boolean>(false)
-  const onClick = () => setOpen(!open)
   return (
-    <div style={{padding: '30px', backgroundColor: 'rgba(0,144,255,0.3)'}}>
+    <AppLayout>
       <h1>Employees Layout</h1>
-      <button onClick={onClick} style={{padding: '10px 20px', marginBottom: '20px', backgroundColor: 'rgba(0,144,255,0.3)'}}>
-        {open ? 'Open' : 'Close'}
-      </button>
       {children}
-    </div>
+    </AppLayout>
   )
 }
